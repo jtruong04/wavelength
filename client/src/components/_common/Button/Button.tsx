739bc2b203c4932a1833cac668201129675d80bd
John Truong <jtruong@buffalo.edu>
2020-12-20 15:53:56 -0500

    Added button component

---
+++ client/src/components/_common/Button/Button.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Button.css';
+export interface ButtonProps {
+    onClick: (event: React.MouseEvent | React.TouchEvent) => void;
+}
+
+export const Button: React.FC<ButtonProps> = ({ onClick, children }) => {
+    const handleClick = (event: React.MouseEvent | React.TouchEvent) => {
+        event.preventDefault();
+        onClick(event);
+    };
+
+    return (
+        <div className='button' onClick={handleClick}>
+            <div>{children}</div>
+        </div>
+    );
+};
+
+export default Button;
